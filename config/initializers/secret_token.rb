0205060372b65562b28a426336e9bf038d960090
Optinovation <hansean@gmail.com>
2013-04-23 00:48:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instafit::Application.config.secret_token = '7e6271c7a962998e94ad76d75190187e397b16350f52668f0cd4240632010d21fd357f8e86509311be7183f746ef1848ff37e92405d87c9df20538ff6f75ed82'
